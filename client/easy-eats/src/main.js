11bd795a795561b0298177f8995bffd83397bef1
unknown <daniel.marin92@hotmail.com>
2023-02-22 05:52:03 -0600

    .

--- client/easy-eats/src/main.js
+++ client/easy-eats/src/main.js
@@ -2,6 +2,7 @@ import { createApp, Vue } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import $ from 'jquery';
 
 /* import the fontawesome core */
 import { library } from '@fortawesome/fontawesome-svg-core'
