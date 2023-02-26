665814fe6a6366e190617d6a7a231910f83878f4
unknown <daniel.marin92@hotmail.com>
2023-02-26 16:20:53 -0600

    Modulo de CATEGORIES FINALIZADO
    Se agregar la API REST del modulo de CATEGORIES.

--- server/EasyEats/categories/sql_strings.py
+++ server/EasyEats/categories/sql_strings.py
@@ -1,2 +1,23 @@
 class Sql_Strings():
     QRY_ALL_CATEGORIES = ("SELECT * FROM categories")
+    
+    QRY_CATEGORY_BY_ID = (
+        "SELECT * FROM categories "
+        "WHERE id = %s"
+    )
+    
+    SQL_CREATE_CATEGORY = (
+        "INSERT INTO categories (name) "
+        "VALUES (%s)"
+    )
+    
+    SQL_UPDATE_CATEGORY = (
+        "UPDATE categories SET "
+        "name = %s "
+        "WHERE id = %s"
+    )
+    
+    SQL_DELETE_CATEGORY = (
+        "DELETE FROM categories "
+        "WHERE id = %s"
+    )
