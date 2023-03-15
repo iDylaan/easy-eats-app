3d8c99b198adabd9972df8e33b9b3058c382a3fe
Adalid10 <123831937+Adalid10@users.noreply.github.com>
2023-03-15 13:51:01 -0600

    se puso editar perfil

---
+++ client/easy-eats/src/views/CrudRecipes.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="">
+    <h1>Recetas</h1>
+    <button @click="Insertar receta">Insertar_receta</button>
+  </div>
+</template>
+
+<script>
+import { useRouter } from 'vue-router';
+
+export default {
+  name: "Home",
+  setup() {
+    const router = useRouter();
+
+    const iniciarSesion = () => {
+      router.push("/login");
+    }
+
+    return {
+      iniciarSesion
+    }
+  }
+}
+</script>
