dda9d3c053882e7fa2e87d8c4ea474611abdbd3b
Danii <daniel.marin92@hotmail.com>
2023-03-22 14:11:17 -0600

    .

--- client/easy-eats/src/components/login-signin/LogoFixed.vue
+++ client/easy-eats/src/components/login-signin/LogoFixed.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="logo-login-signin__container">
-        <img src="../../assets/imgs/icono_fondos_pscuros.png" alt="Logo Easy Eats">
+        <div class="logo__container">
+            <img src="../../assets/imgs/icono_fondos_pscuros.png" alt="Logo Easy Eats">
+        </div>
+        <div class="title__container">
+            <p>Easy Eats</p>
+        </div>
     </div>
 </template>
 <script>
