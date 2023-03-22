ff1bbc244e679473172763f40f9c547622c4f330
brandoncf7 <108553422+brandoncf7@users.noreply.github.com>
2023-03-22 17:18:19 -0600

    .

--- client/easy-eats/src/components/layout/Footer.vue
+++ client/easy-eats/src/components/layout/Footer.vue
@@ -7,7 +7,11 @@
     <div class="box__footer">
         <div class="logo">
             <img src="../../assets/imgs/icono_fondos_pscuros.png" alt="">
+            <!-- <div class="nombre_logo">
+                 <h2>EasyEats</h2>
+            </div> -->
         </div>
+        
         <div class="terms">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas impedit cum cumque velit libero officiis quam doloremque reprehenderit quae corporis! Delectus architecto officia praesentium atque laudantium, nam deleniti sapiente deserunt.</p>
         </div>
@@ -27,15 +31,6 @@
         <a href="#">Lorem, ipsum.</a>
         <a href="#">Lorem, ipsum.</a>              
     </div>
-
-    <!-- <div class="box__footer">
-        <h2>Redes Sociales</h2>
-        <a href="#"> <i class="fab fa-facebook-square"></i> Facebook</a>
-        <a href="#"><i class="fab fa-twitter-square"></i> Twitter</a>
-        <a href="#"><i class="fab fa-linkedin"></i> Linkedin</a>
-        <a href="#"><i class="fab fa-instagram-square"></i> Instagram</a>
-    </div> -->
-
 </div>
 
 <div class="box__copyright">
