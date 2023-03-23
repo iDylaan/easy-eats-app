4a79efd51a373aa823bcdd33a6299986403ba41c
Danii <108493547+iDylaan@users.noreply.github.com>
2023-03-23 15:09:29 -0600

    .

--- client/easy-eats/src/components/login-signin/LogoFixed.vue
+++ client/easy-eats/src/components/login-signin/LogoFixed.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="logo-login-signin__container">
-        <div class="logo__container">
-            <img src="../../assets/imgs/icono_fondos_pscuros.png" alt="Logo Easy Eats">
-        </div>
-        <div class="title__container">
-            <p>Easy Eats</p>
+        <div class="content__contianer">
+            <div class="logo__container">
+                <img src="../../assets/imgs/icono_fondos_pscuros.png" alt="Logo Easy Eats">
+            </div>
+            <div class="title__container">
+                <p>Easy Eats</p>
+            </div>
         </div>
     </div>
 </template>
