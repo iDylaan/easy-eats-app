880731bb748156cf81807aebaf39c2c80f0229de
unknown <daniel.marin92@hotmail.com>
2023-04-03 09:06:17 -0600

    Sacamos la basura

--- client/easy-eats/src/router/index.js
+++ client/easy-eats/src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/HomeView.vue'
 import Loged from '../views/LogedView.vue'
-import Nosotros from '../views/Nosotros.vue'
+import Nosotros from '../views/NosotrosView.vue'
 
 const routes = [
   {
@@ -27,79 +27,7 @@ const routes = [
   {
     path: '/nosotros',
     name: 'nosotros',
-    component: Nosotros
-  },
-  {
-    path: '/preferencias',
-    name: 'preferencias',
-    component: () => import('../views/PreferenciasView.vue')
-  },
-  {
-    path: '/subir-receta',
-    name: 'subir-receta',
-    component: () => import('../views/SubirRecetaView.vue')
-  },
-  {
-    path: '/crudingredients',
-    name: 'crud-ingredientes',
-    component: () => import('../views/IngredientsCrud.vue')
-  },
-  {
-    path: '/editar-perfil',
-    name: 'editar-perfil',
-    component: () => import('../views/EditPerfil.vue')
-  },
-  {
-    path: '/users',
-    name: 'crudusers',
-    component: () => import('../views/CrudUsersView.vue')
-  },
-  {
-    path: '/coments',
-    name: 'coments',
-    component: () => import('../views/ComentsView.vue')
-  },
- 
-  {
-    path: '/ingredients',
-    name: 'ingredients',
-    component: () => import('../views/IngredientsCrud.vue')
-  },
- 
-  {
-    path: '/agreingre',
-    name: 'agregar-ingredientes',
-    component: () => import('../views/AgregarIngrediente.vue')
-  },
-  {
-    path: '/editingre',
-    name: 'editar-ingredientes',
-    component: () => import('../views/EditarIngrediente.vue')
-  },
-  {
-    path: '/deleteingre',
-    name: 'eliminar-ingredientes',
-    component: () => import('../views/BorrarIngrediente.vue')
-  },
-  {
-    path: '/editusers',
-    name: 'editar-usuario',
-    component: () => import('../views/EditUsers.vue')
-  },
-  {
-    path: '/agregaringre',
-    name: 'agregar-ingrediente',
-    component: () => import('../views/AgregarIngrediente.vue')
-  },
-  {
-    path: '/deleteuser',
-    name: 'eliminar-usuario',
-    component: () => import('../views/EliminarUsuario.vue')
-  },
-  {
-    path: '/agregarusers',
-    name: 'agregar-usuario',
-    component: () => import('../views/AgregarUsuario.vue')
+    component: () => import('../views/NosotrosView.vue')
   }
 ]
 
