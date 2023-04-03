c19aa8dad75fd09302ca377a2cbc235f886c258d
brandoncf7 <108553422+brandoncf7@users.noreply.github.com>
2023-04-03 07:58:09 -0600

    .

--- client/easy-eats/src/views/Nosotros.vue
+++ client/easy-eats/src/views/Nosotros.vue
@@ -9,6 +9,7 @@
       </div>
       <div class="vision">
         <h3>Vision</h3>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda rerum et voluptas maxime quod magni laboriosam ullam suscipit tenetur, ducimus exercitationem architecto obcaecati quam similique inventore rem amet illo fugit fuga itaque. Ad dolores eum, iste deleniti ratione, enim quis doloremque soluta veritatis culpa quos ducimus recusandae qui dolorem blanditiis, iure nulla sapiente ut voluptatibus hic maxime quas corporis eligendi tempore. In debitis saepe eligendi! Totam placeat exercitationem corrupti reprehenderit voluptates odit, delectus officiis voluptas itaque ab quibusdam eos fuga quasi! Eius maxime amet nisi enim assumenda quidem, rem, iusto reprehenderit veritatis corrupti cum aliquam unde officiis et, adipisci in.</p>
       </div>
       </div>
     </div>
