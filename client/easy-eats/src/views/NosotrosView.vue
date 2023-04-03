d7ec962325dafc29bfc9b60cef7de76cd8a6f012
unknown <daniel.marin92@hotmail.com>
2023-04-03 13:13:24 -0600

    Registro de Receta estructura

--- client/easy-eats/src/views/NosotrosView.vue
+++ client/easy-eats/src/views/NosotrosView.vue
@@ -22,7 +22,6 @@
 import { useRouter } from 'vue-router';
 import FondoFrutas from '../components/layout/FondoFrutas';
 import Header from '../components/home/Header';
-// import Carrusel from '../components/home/Carrusel';
 import Navbar from '../components/layout/Navbar';
 import Footer from '../components/layout/Footer';
 
