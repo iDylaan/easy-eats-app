f352b9239202c5e6da2c9249b673cd37bccb1653
brandoncf7 <108553422+brandoncf7@users.noreply.github.com>
2023-04-03 16:24:11 -0600

    Perfil de usuario

--- client/easy-eats/src/components/layout/Navbar.vue
+++ client/easy-eats/src/components/layout/Navbar.vue
@@ -37,6 +37,12 @@
         Nueva Receta
     </button>
 
+    <button class="cssbuttons-io-button" @click="perfil"> Perfil
+  <div class="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"></path></svg>
+  </div>
+</button>
+
 </div>
 
 <!-- menu de hamburguesa -->
@@ -58,6 +64,7 @@ export default {
         const goHome = () => router.push('/');
         const nuevaReceta = () => router.push("/subir-receta");
         const goNosotros = () => router.push("/nosotros");
+        const perfil = () => router.push("/detallesPerfil");
 
         const iniciarSesion = () => router.push("/login");
 
@@ -81,6 +88,7 @@ export default {
             goHome,
             isLoged,
             goNosotros,
+            perfil,
         }
     }
 }
