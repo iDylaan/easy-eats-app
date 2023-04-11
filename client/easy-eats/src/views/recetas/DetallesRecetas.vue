f6ae480dbf989b1d23130847edb395f2854c8f61
brandoncf7 <108553422+brandoncf7@users.noreply.github.com>
2023-04-11 12:46:50 -0600

    detallesRecta

--- client/easy-eats/src/views/recetas/DetallesRecetas.vue
+++ client/easy-eats/src/views/recetas/DetallesRecetas.vue
@@ -2,19 +2,33 @@
     <div class="detallesRecetas">
         <Navbar />
        <FondoFrutas />
+        <div class="card-detalles">
+            
+            <div class="nombre-receta">
+                <h1>Pizza</h1>
+            </div>
 
-       <div class="nombre-receta">
-        <h1>Pizza</h1>
-       </div>
+            <div class="img">
+                <img src="../../assets/imgs/pizza.jpg" alt="">
+            </div>
 
-       <div class="img">
-        <img src="../../assets/imgs/pizza.jpg" alt="">
-       </div>
+            <div class="descripcion">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias obcaecati fuga deleniti aperiam dolore cumque, unde odit iste asperiores amet error molestias quae, aut laboriosam ad? Sit sequi voluptates ullam error. Quae repellendus eius, dolores alias rerum quasi, nostrum quo consectetur adipisci quas molestiae cumque nam architecto deserunt iure ratione similique tempore blanditiis mollitia dolorum voluptate a pariatur. Ullam at esse quibusdam! Maiores dolor saepe delectus autem repellat, perspiciatis maxime odio, corporis vitae ipsum non dignissimos officia culpa a tempore commodi magni vero numquam magnam! Ea quod explicabo reiciendis dolores, tenetur quo culpa earum optio harum asperiores facere dicta officia quia iure molestias adipisci nesciunt ab non. Odio, similique! Perferendis esse libero ipsum temporibus ipsam? Architecto aperiam explicabo quae cumque veritatis fuga, accusamus voluptatibus doloribus expedita aliquid, praesentium molestiae asperiores amet dolorum adipisci placeat. Fuga velit, quas voluptate voluptatum magnam ab tempora earum libero dolores debitis temporibus ducimus eaque. Dignissimos!</p>
+            </div>
 
-       <div class="descripcion">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias obcaecati fuga deleniti aperiam dolore cumque, unde odit iste asperiores amet error molestias quae, aut laboriosam ad? Sit sequi voluptates ullam error. Quae repellendus eius, dolores alias rerum quasi, nostrum quo consectetur adipisci quas molestiae cumque nam architecto deserunt iure ratione similique tempore blanditiis mollitia dolorum voluptate a pariatur. Ullam at esse quibusdam! Maiores dolor saepe delectus autem repellat, perspiciatis maxime odio, corporis vitae ipsum non dignissimos officia culpa a tempore commodi magni vero numquam magnam! Ea quod explicabo reiciendis dolores, tenetur quo culpa earum optio harum asperiores facere dicta officia quia iure molestias adipisci nesciunt ab non. Odio, similique! Perferendis esse libero ipsum temporibus ipsam? Architecto aperiam explicabo quae cumque veritatis fuga, accusamus voluptatibus doloribus expedita aliquid, praesentium molestiae asperiores amet dolorum adipisci placeat. Fuga velit, quas voluptate voluptatum magnam ab tempora earum libero dolores debitis temporibus ducimus eaque. Dignissimos!</p>
-       </div>
+            <div class="ingredientes">
+                <h3>Ingredientes</h3>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro, laboriosam voluptas vero recusandae quasi repudiandae enim iure excepturi nesciunt. Eius eos voluptates sequi nulla necessitatibus expedita? Assumenda officiis dolores animi.</p>
+            </div>
 
+            <div class="procedimiento">
+                <h3>Procedimiento</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed eligendi repellendus ea commodi at, non dolores accusantium sunt atque delectus natus quis quam impedit labore velit dicta odit, reiciendis porro!</p>
+            </div>
+       
+        </div>
+
+        <Footer />
         
     </div>
 </template>
