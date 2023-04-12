3dc1e24c4a2326f099b746c2a05a771b5086b391
brandoncf7 <108553422+brandoncf7@users.noreply.github.com>
2023-04-11 20:36:13 -0600

    recetas

---
+++ client/easy-eats/src/components/layout/RecetaCard.vue
@@ -0,0 +1,30 @@
+<template lang="">
+
+    <div class="RecetaCard">
+        <div class="recetas" @click="detallesRecetas">
+        <div class="card">
+        <div class="card-image"></div>
+        <div class="category"> Pizza </div>
+        <div class="heading">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam laborum neque expedita est dolor aliquid modi veritatis temporibus nulla totam, consequuntur laudantium odio ut natus eaque tenetur delectus ipsum in quidem perferendis quis laboriosam dolores at. Repudiandae, sed id. Similique ea consequatur officia, laborum saepe voluptatibus facilis numquam itaque temporibus?
+      </div>
+    </div>
+</template>
+<script>
+import { useRouter } from 'vue-router';
+
+
+export default {
+  name: "RecetaCard",
+  setup() {
+    const router = useRouter();
+    const detallesRecetas = () => router.push("/detallesRecetas");
+
+    return{
+      router,
+      detallesRecetas
+    }
+  },
+  components: {
+  }
+}
+</script>
--- client/easy-eats/src/views/RecetasView.vue
+++ client/easy-eats/src/views/RecetasView.vue
@@ -1,34 +1,30 @@
 <template lang="">
    <div class="recetas">
-    <Navbar />
-    <FondoFrutas />
+      <Navbar />
+      <FondoFrutas />
 
-    <div class="barraBusqueda">
-                <div class="group">
+      <div class="barraBusqueda">
+          <div class="group">
             <svg class="icon" aria-hidden="true" viewBox="0 0 24 24">
-                <g>
-                    <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
-                </g>
+              <g>
+                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+              </g>
             </svg>
-            <input placeholder="Buscar" type="search" class="input">
+              <input placeholder="Buscar" type="search" class="input">
+          </div>
         </div>
-    </div>
 
-    <div class="recetas" @click="detallesRecetas">
-      <div class="card">
-    <div class="card-image"></div>
-    <div class="category"> Pizza </div>
-    <div class="heading">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam laborum neque expedita est dolor aliquid modi veritatis temporibus nulla totam, consequuntur laudantium odio ut natus eaque tenetur delectus ipsum in quidem perferendis quis laboriosam dolores at. Repudiandae, sed id. Similique ea consequatur officia, laborum saepe voluptatibus facilis numquam itaque temporibus?
-        <!-- <div class="author"> By <span class="name">Abi</span> 4 days ago</div> -->
-    </div>
-</div>
-
-
- 
+          <div class="recetas" @click="detallesRecetas">
+            <div class="card">
+          <div class="card-image"></div>
+          <div class="category"> Pizza </div>
+          <div class="heading">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam laborum neque expedita est dolor aliquid modi veritatis temporibus nulla totam, consequuntur laudantium odio ut natus eaque tenetur delectus ipsum in quidem perferendis quis laboriosam dolores at. Repudiandae, sed id. Similique ea consequatur officia, laborum saepe voluptatibus facilis numquam itaque temporibus?
+          </div>
+      </div>
     </div>
 
 
-   </div>
+  </div>
 </template>
 
 <script>
